2555ffe0a8ac86b2508f06dd73de957212fddaa8
Andrés Miguel Bores Hevia <andresmbores@gmail.com>
2017-06-07 09:53:25 -0300

    Added Spring JPA and Apache Derby. Created TopicRepository.

--- src/main/java/com/home/topic/TopicService.java
+++ src/main/java/com/home/topic/TopicService.java
@@ -1,42 +1,38 @@
 package com.home.topic;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-
-	private List<Topic> topics = new ArrayList<>((Arrays.asList(
-			new Topic("music", "Best Music", "Talk about the best music"),
-			new Topic("guitars", "Guitars", "Best guitars"))));
 	
+	@Autowired
+	private TopicRepository topicRepository;
+
 	public List<Topic> getAll() {
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
 	
 	public Optional<Topic> get(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst();
+		return Optional.of(topicRepository.findOne(id));
 	}
 	
 	public void add(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
 	}
 
 	public void update(Topic topic, String id) {
-		for (int i = 0; i < topics.size(); i++) {
-			if (topics.get(i).getId().equals(id)){
-				topics.set(i, topic);
-				return;
-			}
-		}
+		topicRepository.save(topic);
 	}
 	
 	public void delete(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.delete(id);
 	}
 	
 }
